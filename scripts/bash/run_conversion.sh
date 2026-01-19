df0b4f058040c74a943f78373f2061ba4d1681ee
kshitijrajsharma <skshitizraj@gmail.com>
2026-01-19 14:03:12 +0100

    add fractions to run

--- scripts/bash/run_conversion.sh
+++ scripts/bash/run_conversion.sh
@@ -18,7 +18,10 @@ N_EXECUTORS="3"
 SPARK_PACKAGES="ch.cern.sparkmeasure:spark-measure_2.12:0.27"
 
 # Data fraction to process
-FRACTIONS=(0.01 0.03 0.05 0.07 0.10)
+# FRACTIONS=(0.01 0.03 0.05 0.07 0.10)
+
+FRACTIONS=(0.07 0.10)
+
 
 echo "Starting BigEarthNet conversion pipeline"
 echo "Experiment: ${EXPERIMENT_NAME}"
